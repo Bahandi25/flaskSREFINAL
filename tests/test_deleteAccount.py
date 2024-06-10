98e2323ad799b45f89e4fc2b5bb56ae1bf498b32
Jaspettt <KossinovViktor@gmail.com>
2024-06-10 11:54:48 +0500

    srefinal2

--- tests/test_deleteAccount.py
+++ tests/test_deleteAccount.py
@@ -3,7 +3,7 @@ from constants import URL
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto(URL)
